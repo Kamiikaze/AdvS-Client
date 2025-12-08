31d974a4f4cbbf5f3b054393126e2aff74cbcdd4
Kamikaze <timo1322@gmail.com>
2025-12-08 10:15:01 +0100

    fix(electron): correct variable naming in GetStreamUrl action
    Replaced incorrect `episodeHosters` variable with `episodeHoster`.
    Improves clarity and prevents potential logical errors in the module.

--- electron/src/modules/main/action/GetStreamUrl.ts
+++ electron/src/modules/main/action/GetStreamUrl.ts
@@ -19,36 +19,36 @@ export default class GetStreamUrl extends BaseAction<MainDB, MainClient> {
   }>): Promise<ScraperLink | null> {
     const db = this.getModule().getDb();
     this.log('GetStreamUrl', args);
-    const episodeHosters = await this.getModule()
+    const episodeHoster = await this.getModule()
       .getDb()
       .episodeHosters.getObjById(args.episodeHosterId);
 
-    if (!episodeHosters) {
+    if (!episodeHoster) {
       this.log('Episode Host not found', args.episodeHosterId);
       return null;
     }
 
-    if (episodeHosters.hoster_resolved && episodeHosters.resolvedAt) {
+    if (episodeHoster.hoster_resolved && episodeHoster.resolvedAt) {
       const sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
 
-      if (!args.refreshCache && episodeHosters.resolvedAt > sixHoursAgo) {
-        this.log('Using cached streamUrl', episodeHosters.hoster_resolved);
-        return episodeHosters.hoster_resolved;
+      if (!args.refreshCache && episodeHoster.resolvedAt > sixHoursAgo) {
+        this.log('Using cached streamUrl', episodeHoster.hoster_resolved);
+        return episodeHoster.hoster_resolved;
       }
 
       // Reset the resolvedAt date to null to force re-resolution
       this.log('Cached streamUrl is older than 6 hours, resolving again');
-      await db.episodeHosters.updateObject(episodeHosters.e_id, {
+      await db.episodeHosters.updateObject(episodeHoster.e_id, {
         resolvedAt: null,
       });
-      episodeHosters.resolvedAt = null; // Update in memory as well
+      episodeHoster.resolvedAt = null; // Update in memory as well
       this.log('Reset resolvedAt for re-resolution');
     }
 
-    const streamUrl = await scraperWindow(episodeHosters, this);
+    const streamUrl = await scraperWindow(episodeHoster, this);
 
     if (streamUrl?.mediaLink) {
-      await db.episodeHosters.updateObject(episodeHosters.e_id, {
+      await db.episodeHosters.updateObject(episodeHoster.e_id, {
         hoster_resolved: streamUrl,
         resolvedAt: new Date(),
       });
@@ -57,7 +57,7 @@ export default class GetStreamUrl extends BaseAction<MainDB, MainClient> {
       return streamUrl;
     }
 
-    this.log('StreamUrl not found', episodeHosters);
+    this.log('StreamUrl not found', episodeHoster);
     return null;
   }
 }

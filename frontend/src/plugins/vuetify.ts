ccce00e73f9a298fad8f3eb25a150352364d868c
Kamikaze <timo1322@gmail.com>
2025-12-09 18:59:33 +0100

    feat(frontend): add custom theme colors and layout variables
    Introduced reusable CSS variables for layout dimensions.
    Added theme color definitions for better UI consistency.

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -18,6 +18,20 @@ import { de, en } from 'vuetify/locale';
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          'logo-pink': 'rgb(252, 64, 153)',
+          'logo-blue': 'rgb(26, 26, 202)',
+
+          'purple-lighter': 'rgb(100, 59, 201)',
+          'purple-light': 'rgb(82, 33, 188)',
+          'purple-dark': 'rgb(70, 35, 156)',
+
+          'pink-light': 'rgb(156, 39, 176)',
+        },
+      },
+    },
   },
   locale: {
     locale: 'de',

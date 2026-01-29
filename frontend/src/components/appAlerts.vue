9ce7319bf7bdc26c4a37c195ec23cde6278657ff
Kamikaze <timo1322@gmail.com>
2026-01-29 23:58:59 +0100

    fix(frontend): enhance alert component with new props
    Added `title` and `closable` properties to improve alert flexibility.
    Updated alert message and styling for better clarity.

--- frontend/src/components/appAlerts.vue
+++ frontend/src/components/appAlerts.vue
@@ -1,6 +1,12 @@
 <template>
   <div v-for="(alert, index) in alerts" :key="index">
-    <v-alert class="ma-2 py-2" :color="alert.code">{{ alert.message }}</v-alert>
+    <v-alert
+      class="ma-2 py-2"
+      :color="alert.code"
+      :closable="alert.closeable"
+      :title="alert.title"
+      >{{ alert.message }}</v-alert
+    >
   </div>
 </template>
 
@@ -12,9 +18,11 @@ export default defineComponent({
   data: () => ({
     alerts: [
       {
-        code: 'error',
+        code: 'warning',
+        closeable: true,
+        title: 'Hinweis: S.to hat seine Webseiten geändert.',
         message:
-          'Die Seite S.to hatte ein Update, weshalb Serien aktuell nicht funktionieren. Wird schnellstmöglich behoben!',
+          'Die App funktioniert größtenteils wieder, aber einige Features könnten noch nicht korrekt laufen. Ich arbeite bereits an weiteren Fixes – danke für eure Geduld 💙',
       },
     ],
   }),

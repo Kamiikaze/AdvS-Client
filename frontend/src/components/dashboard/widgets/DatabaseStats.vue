68ce30361c2d5f06c18141357b89ef108d6dee60
Kamikaze <timo1322@gmail.com>
2026-04-23 05:32:31 +0200

    feat(frontend): enhance DatabaseStats widget and layout resizing
    Added watch history stats for shows and episodes in DatabaseStats.
    Updated layout dimensions for better usability and alignment.

--- frontend/src/components/dashboard/widgets/DatabaseStats.vue
+++ frontend/src/components/dashboard/widgets/DatabaseStats.vue
@@ -4,6 +4,9 @@
     <v-card-subtitle>Einträge in der Datenbank</v-card-subtitle>
     <v-card-text>
       <v-row dense>
+        <v-col> <h3>Shows</h3> </v-col>
+      </v-row>
+      <v-row dense>
         <v-col> Serien: </v-col>
         <v-col>
           {{ showCount.serien }}
@@ -16,9 +19,18 @@
         </v-col>
       </v-row>
       <v-row dense>
-        <v-col> Gesamt: </v-col>
+        <v-col> <h3>Verlauf</h3> </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col> Shows gesehen: </v-col>
         <v-col>
-          {{ showCount.animes + showCount.serien }}
+          {{ watchHistoryCount.shows }}
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col> Episoden gesehen: </v-col>
+        <v-col>
+          {{ watchHistoryCount.episodes }}
         </v-col>
       </v-row>
     </v-card-text>
@@ -26,28 +38,35 @@
 </template>
 
 <script lang="ts">
+import type { Show } from '@/lib/electron';
 import { useShowStore } from '@/store/show';
 import { mapState } from 'pinia';
 
 export default {
   name: 'DatabaseStats',
+  data: () => ({}),
   computed: {
-    ...mapState(useShowStore, ['shows']),
+    ...mapState(useShowStore, ['shows', 'watchHistory']),
     showCount() {
-      return this.shows.reduce(
-        (count, show) => {
-          if (show.show_type === 'serie') {
-            count.serien += 1;
-          } else if (show.show_type === 'anime') {
-            count.animes += 1;
-          }
-          return count;
-        },
-        { serien: 0, animes: 0 }
+      return {
+        animes: this.shows.filter((show: Show) => show.show_type === 'anime')
+          .length,
+        serien: this.shows.filter((show: Show) => show.show_type === 'serie')
+          .length,
+      };
+    },
+    watchHistoryCount() {
+      const uniqueShows = new Set(
+        this.watchHistory.map((episode) => episode.show_id)
       );
+      const uniqueEpisodes = new Set(
+        this.watchHistory.map((episode) => episode.episode_id)
+      );
+      return {
+        shows: uniqueShows.size,
+        episodes: uniqueEpisodes.size,
+      };
     },
   },
 };
 </script>
-
-<style scoped></style>
